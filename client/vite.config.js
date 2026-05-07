9fc9b560f9147eabaa34b4923b98b3f14b1660dd
sudhams37 <sudhamsreddy3@gmail.com>
2026-05-07 14:20:18 +0530

    fix: remove forced react-refresh plugin in production build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      jsxImportSource: 'react',
-      babel: {
-        plugins: ['react-refresh/babel'],
-      },
-    }),
+    react(),
   ],
   server: {
     host: true,
