893800ca481f85f34e31872213ed1005024ab1c1
sudhams37 <sudhamsreddy3@gmail.com>
2026-05-07 14:41:27 +0530

    fix: resolve Express 5 crash on startup due to wildcard routing

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use('/api/tasks', require('./routes/taskRoutes'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
     
-    app.get('*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'));
     });
 } else {
